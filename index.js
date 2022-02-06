50b73f5b5c504b51feed715c9835c6c2fa28f5e5
Shariful-Islam-Sourav <sorabsorab347@gmail.com>
2022-02-06 12:25:37 +0600

    Updated for Heroku Deploy

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors())
 app.use(express.json());
-const port = 5000;
+const port = process.env.PORT || 9000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3uhme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
